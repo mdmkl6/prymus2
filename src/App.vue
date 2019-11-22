3098d2e24afd5fbb8d0972763637862ff09b149e
Student <student@d17-l427-pc-16.ipa.ki.agh.edu.pl>
2019-11-22 17:37:37 +0100

    1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>  
+    Firstname:<input type="text" v-model="Firstname"></br>
+    Lastname:<input type="text" v-model="Lastname"></br>
+    <button @click="addNew()">Add new</button>
+    <ul v-if="list.length>0">
+      <li v-for="el in list">{{el}}</li>
+    </ul>
+  </div>
+</template>
+ 
+<script>
+    export default {
+        data() {
+            return {list: []}
+        },
+        methods: {
+            addNew(){
+              this.list.push(this.Firstname+" "+this.Lastname);
+            }
+        }
+    };
+</script>
